abe59cf71f6e4b03f9f9ef0f7b61c29ec668141a
Yashrajvaghela <vaghelayashraj122@gmail.com>
2025-07-16 23:05:36 +0530

    Add failing test for deleteSweet feature

--- tests/SweetShop.test.js
+++ tests/SweetShop.test.js
@@ -1,9 +1,27 @@
-
-
-test('should add a sweet to the shop', () => {
+const SweetShop = require("../src/SweetShop");
+ 
+// Add Sweet Feature
+test("should add a sweet to the shop", () => {
   const shop = new SweetShop();
-  const sweet = { id: 1, name: "Kaju Katli", category: "Nut-Based", price: 50, quantity: 20 };
+  const sweet = {
+    id: 1,
+    name: "Kaju Katli",
+    category: "Nut-Based",
+    price: 50,
+    quantity: 20,
+  };
   shop.addSweet(sweet);
   const sweets = shop.getAllSweets();
   expect(sweets).toContainEqual(sweet);
+});
+
+//Delete Sweet Feature
+test('delete a sweet by ID', () => {
+  const shop = new SweetShop();
+  const sweet = { id: 1, name: "Kaju Katli", category: "Nut-Based", price: 50, quantity: 10 };
+  shop.addSweet(sweet);
+
+  shop.deleteSweet(1); 
+
+  expect(shop.getAllSweets()).toEqual([]); 
 });

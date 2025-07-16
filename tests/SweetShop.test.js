a16cb4fa9f6ea03f10aaeaddebb98bbb31127686
Yashrajvaghela <vaghelayashraj122@gmail.com>
2025-07-16 23:18:49 +0530

    Add failing test for view sweets using getAllSweets

--- tests/SweetShop.test.js
+++ tests/SweetShop.test.js
@@ -25,3 +25,19 @@ test('delete a sweet by ID', () => {
 
   expect(shop.getAllSweets()).toEqual([]); 
 });
+
+
+//View All sweets 
+
+test("should return all sweets in the shop", () => {
+  const shop = new SweetShop();
+
+  shop.addSweet({ id: 1, name: "Ladoo", category: "Desi", price: 10, quantity: 50 });
+  shop.addSweet({ id: 2, name: "Barfi", category: "Milk", price: 20, quantity: 30 });
+
+  const allSweets = shop.getAllSweets();
+
+  expect(allSweets).toEqual([
+   
+  ]);
+});
